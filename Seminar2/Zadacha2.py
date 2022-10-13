e1f1a498234e906ab4ed346cacda5e3cc2671d26
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-10-13 12:45:57 +0300

    Create Zadacha2.py

---
+++ Seminar2/Zadacha2.py
@@ -0,0 +1,23 @@
+def InputNum(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+num = InputNum("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
