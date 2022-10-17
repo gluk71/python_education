8242d7622ef01e0b9bef7596a5ef38641ddba571
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-10-17 14:10:01 +0300

    Create Zadacha5.py

---
+++ Seminar2/Zadacha5.py
@@ -0,0 +1,19 @@
+*** Реализуйте алгоритм перемешивания списка.
+/* используем для перемешивания только стандартную библиотеку
+/*
+
+import random
+
+def five():
+	mas = [3, 5, 7, 9, 235, 22]
+	print(mas)
+
+	random.shuffle(mas)
+	print(mas)
+
+	l = len(mas)
+
+	mas2 = random.sample(mas,l)
+	print(mas2)
+		
+five()
