8123a5527882a64039424e3641cfbf5dedd982da
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-10-17 10:45:19 +0300

    Update Zadacha3.py

--- Seminar2/Zadacha3.py
+++ Seminar2/Zadacha3.py
@@ -1,4 +1,26 @@
 
+def InputNum(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+def sumNums(number):
+    s =0
+    for i in range(1,number + 1):
+        n=(1 + 1/i) ** i
+        s = s + n
+
+    return s
+
+number = InputNum("Введите число:....")
+
+print(f"Сумма цифр = {sumNums(number)}")
+
 
 
 
