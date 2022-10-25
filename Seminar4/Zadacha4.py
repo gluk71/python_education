2429c210180bd94027dffcb62b7e9a3494d6c87a
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-10-25 20:31:36 +0300

    Update Zadacha4.py

--- Seminar4/Zadacha4.py
+++ Seminar4/Zadacha4.py
@@ -1,1 +1,42 @@
+/* Задана натуральная степень k. 
+/* Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k. 
 
+import random
+
+def write_file(fname):
+    with open('output.txt', 'w') as data:
+        data.write(fname)
+
+def rnd():
+    return random.randint(-100,101)
+
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koeff = create_mn(k)
+write_file(create_str(koeff))
