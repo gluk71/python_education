6d9a33a98ebbee1b9a6c689ab5870302e19cfed2
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-10-26 06:40:51 +0300

    Update zadacha1.py

--- Seminar5/zadacha1.py
+++ Seminar5/zadacha1.py
@@ -1,1 +1,8 @@
 
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+etner = input("Введите текст через пробел:\n")
+print(f"Исходный текст: {enter}")
+find_txt = "абв"
+lst_txt = [i for i in enter.split() if find_txt not in i]
+print(f'Результат: {" ".join(lst_txt)}')
