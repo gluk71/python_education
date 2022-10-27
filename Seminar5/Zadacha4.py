a8a2a427432ddd269b5802e631abb2f21f935222
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-10-27 06:53:50 +0300

    Update Zadacha4.py

--- Seminar5/Zadacha4.py
+++ Seminar5/Zadacha4.py
@@ -1,2 +1,38 @@
 *** Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 *** Входные и выходные данные хранятся в отдельных текстовых файлах.
+def coding(txt):
+    count = 1
+    res = ''
+    l = len(txt)
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+        
+    file = open("coding.txt", "w")
+    file.write(res)
+    file.close()
+    return res
+
+def decoding(str):
+    number = ''
+    res = ''
+    for i in range(len(str)):
+        if not str[i].isalpha():
+            number += str[i]
+        else:
+            res = res + str[i] * int(number)
+            number = ''
+    file = open("decoding.txt", "w")
+    file.write(res)
+    file.close()
+    return res
+
+with open(r"input.txt", "r") as file:
+    s = file.read()
+print(f"Кодирование: {coding(s)}")
+print(f"Декодирование: {decoding(coding(s))}")
